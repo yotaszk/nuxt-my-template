253158f34827ce3824c0b897968d014add9203a0
Yota Suzuki <yotaszk@gmail.com>
2019-10-13 15:53:18 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    { src: 'node_modules/modern-normalize/modern-normalize.css' }
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/google-analytics',
+    '@nuxtjs/sitemap'
+  ],
+
+  googleAnalytics: {
+    id: 'UA-XXXXXXXXXX-X'
+  },
+
+  styleResources: {
+    scss: [
+      '~/assets/scss/XXXXXX.scss'
+    ]
+  },
+
+  sitemap: {},
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  },
+  generate: {
+    routes: [
+      '/xxxxxxxxxx'
+    ]
+  }
+}
